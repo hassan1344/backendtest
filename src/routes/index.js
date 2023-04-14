4454090ead38b3db7bc9acd4f01984502838d9e2
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2023-04-15 02:13:22 +0500

    added a comment in route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,8 +12,10 @@ const router = express.Router();
 router.post("/survivor/add-survivor", add_survivor);
 router.put("/survivor/update-location/:id", update_location);
 router.put("/survivor/infect-survivor/:id", infect_survivor);
+router.post("/survivor/trade-items", trade_items);
+
+//get reports
 router.get("/survivor/get-percentage", get_infected_noninfected_percentage);
 router.get("/survivor/get-average-items", get_average_items);
-router.post("/survivor/trade-items", trade_items);
 router.get("/survivor/get-points-lost/:id", get_points_lost);
 export default router;
