cf9b3142d50a3ce616879ec68d5fd034db50372c
Muhammad Hassan <60579417+hassan1344@users.noreply.github.com>
2023-04-15 01:06:36 +0500

    controller added for get infected and noninfected percentage

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import { add_survivor } from "../services/addSurvivor.js";
 import { update_location } from "../services/updateLocation.js";
 import { infect_survivor } from "../services/infectSurvivor.js";
+import { get_infected_noninfected_percentage } from "../services/getPercentage.js";
 
 const router = express.Router();
 
 router.post("/survivors", add_survivor);
 router.put("/survivor/update-location/:id", update_location);
 router.put("/survivor/infect-survivor/:id", infect_survivor);
-
+router.get("/survivor/get-percentage", get_infected_noninfected_percentage);
 export default router;
